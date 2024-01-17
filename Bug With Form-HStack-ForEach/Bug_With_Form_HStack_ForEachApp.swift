59e72ea422f39c976f6ca75a8e682c3ca950f4a6
Monty Harper <132530279+MontyHarper@users.noreply.github.com>
2024-01-17 16:24:11 -0600

    Initial Commit

---
+++ Bug With Form-HStack-ForEach/Bug_With_Form_HStack_ForEachApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bug_With_Form_HStack_ForEachApp.swift
+//  Bug With Form-HStack-ForEach
+//
+//  Created by Monty Harper on 1/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Bug_With_Form_HStack_ForEachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bug With Form-HStack-ForEach/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Bug With Form-HStack-ForEach
+//
+//  Created by Monty Harper on 1/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
